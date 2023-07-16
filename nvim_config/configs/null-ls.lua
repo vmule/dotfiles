ee946edabdd53548920eb44610a4e3851d3444b9
Vito Mule <mulevito@gmail.com>
2023-07-15 17:07:39 -0700

    add bash linter

--- nvim_config/configs/null-ls.lua
+++ nvim_config/configs/null-ls.lua
@@ -6,6 +6,8 @@ local opts = {
     null_ls.builtins.formatting.black,
     null_ls.builtins.diagnostics.mypy,
     null_ls.builtins.diagnostics.ruff,
+    null_ls.builtins.diagnostics.ruff,
+    null_ls.builtins.diagnostics.shellcheck,
   },
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
