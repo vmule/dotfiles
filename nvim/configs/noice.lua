e59fa6e97d36ea11c9b8df8aff9668c318612737
Vito Mule <mulevito@gmail.com>
2023-07-20 20:34:34 -0700

    Change cmd_line ui position

--- nvim/configs/noice.lua
+++ nvim/configs/noice.lua
@@ -15,10 +15,43 @@ require("noice").setup({
   },
   -- you can enable a preset for easier configuration
   presets = {
-    bottom_search = true, -- use a classic bottom cmdline for search
-    command_palette = true, -- position the cmdline and popupmenu together
+    bottom_search = false, -- use a classic bottom cmdline for search
     long_message_to_split = true, -- long messages will be sent to a split
     inc_rename = false, -- enables an input dialog for inc-rename.nvim
-    lsp_doc_border = false, -- add a border to hover docs and signature help
-  },
+    lsp_doc_border = true, -- add a border to hover docs and signature help
+    command_palette = {
+      views = {
+        cmdline_popup = {
+          position = {
+            row = "30%",
+            col = "50%",
+          },
+          size = {
+            min_width = 85,
+            width = "auto",
+            height = "auto",
+          },
+        },
+        cmdline_popupmenu = {
+          relative = "editor",
+          position = {
+            row  = "30%",
+            col = "50%",
+          },
+          size = {
+            width = 85,
+            height = "auto",
+            max_height = 15,
+          },
+          border = {
+            style = "rounded",
+            padding = { 0, 1 },
+          },
+          win_options = {
+            winhighlight = { Normal = "Normal", FloatBorder = "NoiceCmdlinePopupBorder" },
+          },
+        },
+      },
+    },
+  }
 })
