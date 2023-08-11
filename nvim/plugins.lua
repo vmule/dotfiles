cb2eaa536f6e99ccbc6839b78a0d3a71881f41c3
Vito Mule <mulevito@gmail.com>
2023-08-11 00:48:44 -0700

    Cleanup plugins

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -229,26 +229,12 @@ local plugins = {
 	{
 		"chrisgrieser/nvim-early-retirement",
 		config = true,
-		event = "VeryLazy",
-		lazy = true,
+		lazy = false,
 	},
 	{
 		"chrisgrieser/nvim-origami",
 		event = "BufReadPost", -- later or on keypress would prevent saving folds
 		opts = true, -- needed even when using default config
-	},
-	{
-		"Zeioth/markmap.nvim",
-		build = "yarn global add markmap-cli",
-		cmd = { "MarkmapOpen", "MarkmapSave", "MarkmapWatch", "MarkmapWatchStop" },
-		opts = {
-			html_output = "~/workspace/jobsearch/markmap.html", -- (default) Setting a empty string "" here means: [Current buffer path].html
-			hide_toolbar = false, -- (default)
-			grace_period = 3600000, -- (default) Stops markmap watch after 60 minutes. Set it to 0 to disable the grace_period.
-		},
-		config = function(_, opts)
-			require("markmap").setup(opts)
-		end,
 	},
 	{
 		"gaborvecsei/usage-tracker.nvim",
@@ -265,6 +251,19 @@ local plugins = {
 		end,
 		lazy = false,
 	},
+	-- {
+	-- 	"Zeioth/markmap.nvim",
+	-- 	build = "yarn global add markmap-cli",
+	-- 	cmd = { "MarkmapOpen", "MarkmapSave", "MarkmapWatch", "MarkmapWatchStop" },
+	-- 	opts = {
+	-- 		html_output = "~/workspace/jobsearch/markmap.html", -- (default) Setting a empty string "" here means: [Current buffer path].html
+	-- 		hide_toolbar = false, -- (default)
+	-- 		grace_period = 3600000, -- (default) Stops markmap watch after 60 minutes. Set it to 0 to disable the grace_period.
+	-- 	},
+	-- 	config = function(_, opts)
+	-- 		require("markmap").setup(opts)
+	-- 	end,
+	-- },
 	-- {
 	-- 	"tamton-aquib/zone.nvim",
 	-- 	config = function()
